9109924d4d5997ae08a8a7bcf05c82cf48829909
Your Name <marielmetalios@gmail.com>
2025-02-20 16:13:42 -0500

    adding changes to deploy

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,5 +1,13 @@
 import mongoose from 'mongoose';
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+
+const mongoURI = process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks';
+
+
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
 
 export default mongoose.connection;
--- server/src/server.ts
+++ server/src/server.ts
@@ -9,13 +9,27 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('/client/dist'));
-}
+// // if we're in production, serve client/build as static assets
+// if (process.env.NODE_ENV === 'production') {
+//   app.use(express.static('/client/dist'));
+// }
+
+const dbUri = process.env.NODE_ENV === 'production' ? process.env.MONGO_URI && app.use(express.static('/client/dist')) : process.env.MONGODB_URI;
 
 app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
+
+
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+mongoose.connect(dbUri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((error: any) => console.error('Error connecting to MongoDB:', error));
+
