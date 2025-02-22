09436bc0d71ea528e53186249783165c0f48d667
Your Name <marielmetalios@gmail.com>
2025-02-22 07:56:57 -0500

    updating

--- server/src/server.ts
+++ server/src/server.ts
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// if we're in PROD, serve client/build as static assets
+// if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/dist'));
 }
